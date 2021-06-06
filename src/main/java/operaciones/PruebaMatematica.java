c84daccbd24f4c04ed11ad80d87365bd4171d25f
Keily Vallejo <kmvallejo2@espe.edu.ec>
2021-06-06 07:55:21 -0500

    v1 clase matematica

---
+++ src/main/java/operaciones/PruebaMatematica.java
@@ -0,0 +1,17 @@
+//esta clase permite instanciar los objetos de clase matematica
+package operaciones;
+
+
+public class PruebaMatematica {
+    
+    public static void main(String[] args) {
+        //definicion de variable de tipo matematica, es una variable local 
+        matematica ob1 = new matematica();
+        ob1.a=25;
+        ob1.b=35;
+        ob1.sumar();
+        
+        // se puede utilizar var 
+            var a="buenos dias";
+    }
+}
---
+++ src/main/java/operaciones/matematica.java
@@ -0,0 +1,18 @@
+
+package operaciones;
+
+
+public class matematica {
+    //atrinujos de la clase matematica
+    int a;
+    int b;
+    
+    //método de la clase matemáticas
+    public void sumar(){
+    
+        //variables locales
+        int resultado=a + b;
+        System.out.println("EL resultado es: " + resultado);
+        
+    }
+}
